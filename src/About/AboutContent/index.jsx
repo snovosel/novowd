cb316befe68e6b4d2c43633a0d50e54c9bf4d821
snovosel <novosel991@gmail.com>
2018-10-23 23:13:43 -0400

    finished about

--- src/About/AboutContent/index.jsx
+++ src/About/AboutContent/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import CSSModules from 'react-css-modules';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+import { FaArrowLeft } from 'react-icons/fa';
 
 import styles from './index.style.scss';
 
@@ -8,11 +10,16 @@ class AboutContent extends Component {
   render() {
     return (
       <div styleName="header">
+        <div styleName="back">
+          <Link to="/">
+            <FaArrowLeft size="52" color="white" />
+          </Link>
+        </div>
         <div styleName="list">
           <ul>
-            <li>name: Steve Novosel</li>
-            <li>current location: Philadelphia</li>
-            <li>current role: React Engineer</li>
+            <li>name: steve novosel</li>
+            <li>current location: philadelphia</li>
+            <li>current role: react engineer</li>
           </ul>
         </div>
         <div styleName="bio">
@@ -21,7 +28,8 @@ class AboutContent extends Component {
           it is entirely based upon <b>ideas</b>, rather than resources. With the power of <b>ideas</b>, software has tackled some of modern life's
           most common struggles. Buying things online has never been this easy. Neither has getting a ride, doing your taxes, keeping track of birthdays, events, and anniverseries.
           Budgeting can be done smoothly using beautiful and sleek user interfaces, and any movie or tv show ever filmed on earth (not to mention the millions upon millions of
-          user-uploaded videos) can be accessed in a matter of minutes. Software is a tool,
+          user-uploaded videos) can be accessed in a matter of minutes. Software is a tool. It can make a lot of money, but all it takes is an <b>idea</b>. My goal is to take those
+          ideas and turn them into a reality. With some vision and a solid idea, we can make something great. 
           </p>
         </div>
       </div>
