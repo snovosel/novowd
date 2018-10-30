020223ca42a8962c42eeb9006993addf8252d6ce
snovosel <novosel991@gmail.com>
2018-10-30 18:45:18 -0400

    rough draft contact

--- src/Contact/ContactContent/index.jsx
+++ src/Contact/ContactContent/index.jsx
@@ -11,49 +11,61 @@ class ContactContent extends Component {
   render() {
     return (
       <div styleName="content">
-        <Link to="/">
-          <div styleName="back">
-            <FaArrowLeft size="40" color="#74b4be"  />
+        <div styleName="top">
+          <Link to="/">
+            <div styleName="back">
+              <FaArrowLeft size="40" color="#74b4be"  />
+            </div>
+          </Link>
+          <div styleName="header">
+            <p>Send me a message, ask a question, or find out more about what I can offer you and your <b>ideas</b>.</p>
           </div>
-        </Link>
-        <div styleName="header">
-          <p>Send me a message, ask a question, or find out more about what I can offer you and your <b>ideas</b>.</p>
         </div>
-        <div styleName="form">
-          <div styleName="inputs">
-            <div styleName="left">
-              <div styleName="left-container">
-                <p styleName="contact-header">tell us about yourself</p>
-                <div styleName="input">
-                  <span styleName="icon-container"><span styleName="icon"><FaAt /></span></span>
-                  <input type="text" />
+        <div styleName="bottom">
+          <div styleName="form">
+            <div styleName="inputs">
+              <div styleName="left">
+                <div styleName="left-container">
+                  <div styleName="contact-header"><p>tell us about yourself</p></div>
+                  <div styleName="input-container">
+                    <div styleName="input">
+                      <span styleName="icon-container"><span styleName="icon"><FaAt /></span></span>
+                      <input type="text" />
+                    </div>
+                    <div styleName="input">
+                      <span styleName="icon-container"><span styleName="icon"><FaHashtag /></span></span>
+                      <input type="text" />
+                    </div>
+                    <div styleName="input">
+                      <span styleName="icon-container"><span styleName="icon"><FaQuestion /></span></span>
+                      <select styleName="select">
+                        <option styleName="option" disabled selected value />
+                        <option styleName="option" value="I have an idea">I have an idea</option>
+                        <option styleName="option" value="I want to learn how to code">I want to learn how to code</option>
+                      </select>
+                    </div>
+                  </div>
                 </div>
-                <div styleName="input">
-                  <span styleName="icon-container"><span styleName="icon"><FaHashtag /></span></span>
-                  <input type="text" />
-                </div>
-                <div styleName="input">
-                  <span styleName="icon-container"><span styleName="icon"><FaQuestion /></span></span>
-                  <input type="text" />
+              </div>
+              <div styleName="right">
+                <div styleName="right-container">
+                  <div styleName="message-header"><p>send a message...</p></div>
+                  <div styleName="message-container">
+                    <div styleName="message" contentEditable="true" />
+                  </div>
                 </div>
               </div>
             </div>
-            <div styleName="right">
-              <div styleName="message" contentEditable="true">
-                <p styleName="text">send us a message...</p>
-              </div>
-            </div>
+            <button styleName="submit">
+              <p>submit</p>
+            </button>
           </div>
-          <button styleName="submit">
-            <p>submit</p>
-          </button>
         </div>
+        <div styleName="buffer" />
       </div>
     );
   }
 }
-
-
 
 
 const ContactContentStyled = CSSModules(ContactContent, styles, {
