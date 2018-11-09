b5ce6c1c1ddabd2c020bd5bb7b89b89d30928fb2
snovosel <novosel991@gmail.com>
2018-11-09 18:56:20 -0500

    added shortcut icon

--- src/About/AboutContent/index.jsx
+++ src/About/AboutContent/index.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import CSSModules from 'react-css-modules';
-import { Link } from 'react-router-dom';
 
 import BackButton from "components/Navigation/BackButton/index.jsx";
 import Bio from "./Bio/index.jsx";
 
-import { FaArrowLeft, FaChessPawn } from 'react-icons/fa';
+import { FaChessPawn } from 'react-icons/fa';
 
 import styles from './index.style.scss';
 
